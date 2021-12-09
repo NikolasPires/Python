e6c13ac569ffcd08a5c9b5d541cc9ca494abd5e9
Nikolas Pires <90667041+NikolasPires@users.noreply.github.com>
2021-12-09 17:15:56 -0300

    Delete desafios 100 a x directory

--- desafios 100 a x/ex114.py
+++
@@ -1,9 +0,0 @@
-import urllib.request
-
-try:
-    site = urllib.request.urlopen('http://www.pudim.com.br')
-except urllib.error.URLError:
-    print('Deu erro')
-else:
-    print('Tudo OK consegui acessar')
-    print(site.read())
